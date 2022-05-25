d515d6371a10315e82f64f0b4930b02db9de2706
Linh2402 <nthoailinh2402@gmail.com>
2022-05-25 23:03:39 +0700

    create-note-and-top-page

--- website/src/views/pages/register/index.js
+++ website/src/views/pages/register/index.js
@@ -1,106 +1,109 @@
-import { useNavigate } from 'react-router-dom'
-import { Form, Input, Button, Select } from 'antd'
+import { useNavigate } from 'react-router-dom';
+import { Form, Input, Button, Select } from 'antd';
 import {
-    UserOutlined,
-    UnlockOutlined,
-    MailOutlined,
-    SettingOutlined,
-} from '@ant-design/icons'
-import messages from 'assets/lang/messages'
-import auth from 'api/auth'
+  UserOutlined,
+  UnlockOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import messages from 'assets/lang/messages';
+import auth from 'api/auth';
 
-import background from 'assets/images/background.png'
-import avatar from 'assets/images/avatar.svg'
+import background from 'assets/images/background.png';
+import avatar from 'assets/images/avatar.svg';
 
-import './register.scss'
+import './register.scss';
 
-const { Option } = Select
+const { Option } = Select;
 
 function Register() {
-    const navigate = useNavigate()
-    const handleSubmit = async (values) => {
-        try {
-            values.role = parseInt(values.role)
-            const response = await auth.register(values)
-            alert(response.data.message)
-            navigate('/login')
-        } catch (error) {
-            //TODO: hiển bị thông báo theo từng error code (error.request.status === 404)
-            alert(error.response.data.message)
-        }
-    }
-    return (
-        <div className="register-container-main">
-            <div className="register-card">
-                <div className="register-img-background">
-                    <img
+  const navigate = useNavigate();
+  const handleSubmit = async (values) => {
+    // try {
+    //   values.role = parseInt(values.role);
+    //   const response = await auth.register(values);
+    //   alert(response.data.message);
+    //   navigate('/login');
+    // } catch (error) {
+    //   //TODO: hiển bị thông báo theo từng error code (error.request.status === 404)
+    //   alert(error.response.data.message);
+    // }
+    console.log(values);
+  };
+  return (
+    <div className="register-container-main">
+      <div className="register-card">
+        <div className="register-img-background">
+          {/* <img
                         className="img-background"
                         src={background}
                         alt={'backgound'}
-                    />
-                </div>
-                <div className="register-container-sub">
-                    <div className="register-content"></div>
-                    <div className="register-form-content">
-                        <Form
-                            name="register"
-                            className="register-form"
-                            onFinish={handleSubmit}
-                        >
-                            <img src={avatar} alt={'avatar'} />
-                            <h2>Welcome</h2>
+                    /> */}
+          <h1>シンプルノート</h1>
+          <p>外国語をより簡単に学ぶ。</p>
+        </div>
+        <div className="register-container-sub">
+          <div className="register-content"></div>
+          <div className="register-form-content">
+            <Form
+              name="register"
+              className="register-form"
+              onFinish={handleSubmit}
+            >
+              <img src={avatar} alt={'avatar'} />
+              <h2>Welcome</h2>
 
-                            <div className="input-div name">
-                                <i>
-                                    <UserOutlined />
-                                </i>
-                                <Form.Item
-                                    className="form-item"
-                                    name="name"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: messages['name_required'],
-                                        },
-                                    ]}
-                                >
-                                    <Input
-                                        type="name"
-                                        placeholder="Username"
-                                        size="large"
-                                        className="input name"
-                                    />
-                                </Form.Item>
-                            </div>
+              <div className="input-div name">
+                <i>
+                  <UserOutlined />
+                </i>
+                <Form.Item
+                  className="form-item"
+                  name="name"
+                  rules={[
+                    {
+                      required: true,
+                      message: messages['name_required'],
+                    },
+                  ]}
+                >
+                  <Input
+                    type="name"
+                    placeholder="Username"
+                    size="large"
+                    className="input name"
+                  />
+                </Form.Item>
+              </div>
 
-                            <div className="input-div email">
-                                <i>
-                                    <MailOutlined />
-                                </i>
-                                <Form.Item
-                                    className="form-item"
-                                    name="email"
-                                    rules={[
-                                        {
-                                            type: 'email',
-                                            message: messages['invalid_email'],
-                                        },
-                                        {
-                                            required: true,
-                                            message: messages['email_required'],
-                                        },
-                                    ]}
-                                >
-                                    <Input
-                                        type="email"
-                                        placeholder="Email"
-                                        size="large"
-                                        className="input email"
-                                    />
-                                </Form.Item>
-                            </div>
+              <div className="input-div email">
+                <i>
+                  <MailOutlined />
+                </i>
+                <Form.Item
+                  className="form-item"
+                  name="email"
+                  rules={[
+                    {
+                      type: 'email',
+                      message: messages['invalid_email'],
+                    },
+                    {
+                      required: true,
+                      message: messages['email_required'],
+                    },
+                  ]}
+                >
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    size="large"
+                    className="input email"
+                  />
+                </Form.Item>
+              </div>
 
-                            <div className="input-div role">
+              {/* <div className="input-div role">
                                 <i>
                                     <SettingOutlined />
                                 </i>
@@ -124,101 +127,85 @@ function Register() {
                                         </Option>
                                     </Select>
                                 </Form.Item>
-                            </div>
+                            </div> */}
 
-                            <div className="input-div password">
-                                <i>
-                                    <UnlockOutlined />
-                                </i>
-                                <Form.Item
-                                    className="form-item"
-                                    name="password"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message:
-                                                messages['password_required'],
-                                        },
-                                        {
-                                            type: 'string',
-                                            min: 6,
-                                            max: 24,
-                                            message:
-                                                messages[
-                                                    'invalid_password_length'
-                                                ],
-                                        },
-                                    ]}
-                                >
-                                    <Input.Password
-                                        placeholder="Password"
-                                        className="input password"
-                                    />
-                                </Form.Item>
-                            </div>
+              <div className="input-div password">
+                <i>
+                  <UnlockOutlined />
+                </i>
+                <Form.Item
+                  className="form-item"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: messages['password_required'],
+                    },
+                    {
+                      type: 'string',
+                      min: 6,
+                      max: 24,
+                      message: messages['invalid_password_length'],
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    placeholder="Password"
+                    className="input password"
+                  />
+                </Form.Item>
+              </div>
 
-                            <div className="input-div confirm-password">
-                                <i>
-                                    <UnlockOutlined />
-                                </i>
-                                <Form.Item
-                                    name="confirm-password"
-                                    dependencies={['password']}
-                                    hasFeedback
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message:
-                                                messages[
-                                                    'confirm_password_require'
-                                                ],
-                                        },
-                                        ({ getFieldValue }) => ({
-                                            validator(_, value) {
-                                                if (
-                                                    !value ||
-                                                    getFieldValue(
-                                                        'password',
-                                                    ) === value
-                                                ) {
-                                                    return Promise.resolve()
-                                                }
-                                                return Promise.reject(
-                                                    new Error(
-                                                        messages[
-                                                            'confirm_password_not_match'
-                                                        ],
-                                                    ),
-                                                )
-                                            },
-                                        }),
-                                    ]}
-                                >
-                                    <Input.Password
-                                        placeholder="Confirm password"
-                                        className="input confirm-password"
-                                    />
-                                </Form.Item>
-                            </div>
-                            <Button
-                                className="button-submit"
-                                type="primary"
-                                htmlType="submit"
-                            >
-                                REGISTER
-                            </Button>
-                            <p className="have-an-account">
-                                Already have an account?{' '}
-                                <a className="have-an-account" href="/login">
-                                    {' '}
-                                    Login now
-                                </a>
-                            </p>
-                        </Form>
-                    </div>
-                </div>
-            </div>
+              <div className="input-div confirm-password">
+                <i>
+                  <UnlockOutlined />
+                </i>
+                <Form.Item
+                  name="confirm-password"
+                  dependencies={['password']}
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: messages['confirm_password_require'],
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        if (!value || getFieldValue('password') === value) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          new Error(messages['confirm_password_not_match'])
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password
+                    placeholder="Confirm password"
+                    className="input confirm-password"
+                  />
+                </Form.Item>
+              </div>
+              <Button
+                className="button-submit"
+                type="primary"
+                htmlType="submit"
+              >
+                REGISTER
+              </Button>
+              <p className="have-an-account">
+                Already have an account?{' '}
+                <a className="have-an-account" href="/login">
+                  {' '}
+                  Login now
+                </a>
+              </p>
+            </Form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Register
+export default Register;
--- website/src/views/routes/index.js
+++ website/src/views/routes/index.js
@@ -1,52 +1,63 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import MainLayout from 'components/layouts/MainLayout'
-import loadableComponent from 'components/loadable-component'
-import AuthenticatedRoute from './authenticated-route'
-import GuestRoute from './guest-route'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from 'components/layouts/MainLayout';
+import loadableComponent from 'components/loadable-component';
+import AuthenticatedRoute from './authenticated-route';
+import GuestRoute from './guest-route';
 
-const Login = loadableComponent(() => import('views/pages/login'))
-const Register = loadableComponent(() => import('views/pages/register'))
-const Profile = loadableComponent(() => import('views/pages/profile'))
+const Login = loadableComponent(() => import('views/pages/login'));
+const Register = loadableComponent(() => import('views/pages/register'));
+const Profile = loadableComponent(() => import('views/pages/profile'));
 const EditProfile = loadableComponent(() =>
-    import('views/pages/profile/edit-profile'),
-)
+  import('views/pages/profile/edit-profile')
+);
 const ChangePassword = loadableComponent(() =>
-    import('views/pages/change-password'),
-)
+  import('views/pages/change-password')
+);
+const CreateNote = loadableComponent(() => import('views/pages/create-note'));
+const Toppage = loadableComponent(() => import('views/pages/top-page'));
 
-const NotFound = loadableComponent(() => import('views/pages/404-not-found'))
-const availableRoles = [1, 2]
+const NotFound = loadableComponent(() => import('views/pages/404-not-found'));
+const availableRoles = [1, 2];
 // const BASIC_USER_ROLE = 0;
 // const PARKING_LOT_USER_ROLE = 1;
 // const ADMIN_ROLE = 2;
 
 function AllRoutes() {
-    return (
-        <Routes>
-            <Route element={<GuestRoute />}>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Route>
+  return (
+    <Routes>
+      <Route element={<GuestRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/change-password"
+          element={<MainLayout component={ChangePassword} />}
+        />
+        <Route path="/profile" element={<MainLayout component={Profile} />} />
+        <Route
+          path="/profile/edit"
+          element={<MainLayout component={EditProfile} />}
+        />
+        <Route
+          path="/create-note"
+          element={<MainLayout component={CreateNote} />}
+        />
+        <Route path="/toppage" element={<MainLayout component={Toppage} />} />
+      </Route>
 
-            <Route
-                element={<AuthenticatedRoute acceptedRoles={availableRoles} />}
-            >
-                <Route
-                    path="/change-password"
-                    element={<MainLayout component={ChangePassword} />}
-                />
-                <Route
-                    path="/profile"
-                    element={<MainLayout component={Profile} />}
-                />
-                <Route
-                    path="/profile/edit"
-                    element={<MainLayout component={EditProfile} />}
-                />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    )
+      <Route element={<AuthenticatedRoute acceptedRoles={availableRoles} />}>
+        <Route
+          path="/change-password"
+          element={<MainLayout component={ChangePassword} />}
+        />
+        <Route path="/profile" element={<MainLayout component={Profile} />} />
+        <Route
+          path="/profile/edit"
+          element={<MainLayout component={EditProfile} />}
+        />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
-export default AllRoutes
+export default AllRoutes;
