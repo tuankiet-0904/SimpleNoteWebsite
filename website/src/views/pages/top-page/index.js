8a4616406a50b1ac5fda0df2cfd84ac44cd99f02
Nguyễn Đặng Tuấn Kiệt <tuankietnk2001@gmail.com>
2022-05-27 23:51:42 +0700

    Merge branch add category

--- website/src/views/pages/top-page/index.js
+++ website/src/views/pages/top-page/index.js
@@ -1,81 +1,11 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import './toppage.scss'
-function Toppage() {
-    const listnode = [
-        {
-            id: 1,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 2,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 3,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 4,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 5,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 6,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 7,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-        {
-            id: 8,
-            keyword: 'lorem',
-            memo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, iure!',
-        },
-    ]
-    return (
-        <div className="container">
-            <div className="wrapper">
-                <h1 style={{ fontSize: '20px' }}>Category 1</h1>
-                <div className="list-note">
-                    {listnode.map((item) => (
-                        <div className="note-item" key={item.id}>
-                            <div className="inner">
-                                <div className="front">
-                                    <h1>{item.keyword}</h1>
-                                </div>
-                                <div className="back">
-                                    <p>{item.memo}</p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-=======
-import './toppage.scss';
+
 import { PlusCircleOutlined } from '@ant-design/icons'
 import { Link, useNavigate } from 'react-router-dom'
-
-function Toppage() {
-=======
 import './toppage.scss';
-import { PlusCircleOutlined } from '@ant-design/icons'
-import { Link, useNavigate } from 'react-router-dom'
 
 function Toppage() {
->>>>>>> Stashed changes
   const navigate = useNavigate()
-  const onClickHandler = () => navigate('/top-page/add')
+  const onClickHandler = () => navigate('/categories/add')
 
   const listnode = [
     {
@@ -136,10 +66,6 @@ function Toppage() {
               <div className="inner">
                 <div className="front">
                   <h1>{item.keyword}</h1>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                 </div>
                 <div className="back">
                   <p>{item.memo}</p>
